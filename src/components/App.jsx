16057663c2c55e243e90a20dcbd59dd7de881e27
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-01-05 17:02:58 +0200

    StepTwo commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,30 @@ import ContactList from './ContactList';
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const addContact = () => {
     const newContact = {
       id: nanoid(),
       name: name,
+      number: number,
     };
 
     setContacts(prevContacts => [...prevContacts, newContact]);
     setName('');
+    setNumber('');
   };
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm name={name} setName={setName} addContact={addContact} />
+      <ContactForm
+        name={name}
+        setName={setName}
+        number={number}
+        setNumber={setNumber}
+        addContact={addContact}
+      />
       <ContactList contacts={contacts} />
     </div>
   );
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import classes from './ContactForm.module.css';
 
-const ContactForm = ({ name, setName, addContact }) => {
+const ContactForm = ({ name, setName, number, setNumber, addContact }) => {
   const handleNameChange = event => {
     setName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
   };
 
   const handleSubmit = event => {
@@ -11,22 +16,36 @@ const ContactForm = ({ name, setName, addContact }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={name}
-          onChange={handleNameChange}
-        />
-      </label>
-      <button type="submit">Add Contact</button>
+    <div className={classes.contactForm}>
+      <form className={classes.contactFormForm} onSubmit={handleSubmit}>
+        <label className={classes.contactFormLabel}>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={handleNameChange}
+          />
+        </label>
+        <label className={classes.contactFormLabel}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={handleNumberChange}
+          />
+        </label>
+        <button type="submit">Add Contact</button>
+      </form>
       <h2>Contacts</h2>
-    </form>
+    </div>
   );
 };
 
